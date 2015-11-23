c52ba8252ee9ca6952ff68f068c029ecd4c99527
Pierre Arnaud <arnaud@epsitec.ch>
2015-11-23 06:54:46 +0100

    Add NPM compilation step and distribute transpiled ES5 code.

---
+++ src/index.js
@@ -0,0 +1,11 @@
+'use strict';
+
+import Activity from './store/activity.js';
+import Node from './store/node.js';
+import Store from './store/store.js';
+
+module.exports = {
+  Activity,
+  Node,
+  Store
+};
