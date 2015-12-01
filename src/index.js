d6936c662dcdf1109158ef0bf1ddc2cfa920ea7e
Pierre Arnaud <arnaud@epsitec.ch>
2015-12-01 11:32:50 +0100

    Remove middleware.

--- src/index.js
+++ src/index.js
@@ -1,13 +1,11 @@
 'use strict';
 
 import Activity from './store/activity.js';
-import Middleware from './store/middleware.js';
 import State from './store/state.js';
 import Store from './store/store.js';
 
 module.exports = {
   Activity,
-  Middleware,
   State,
   Store
 };
