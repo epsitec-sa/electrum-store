c984ff6e88db3fe8d2f9c99dd279aeaf5b91d3a3
Pierre Arnaud <arnaud@epsitec.ch>
2016-02-05 13:28:23 +0100

    Remove class Activity.

--- src/index.js
+++ src/index.js
@@ -1,11 +1,9 @@
 'use strict';
 
-import Activity from './store/activity.js';
 import State from './store/state.js';
 import Store from './store/store.js';
 
 module.exports = {
-  Activity,
   State,
   Store
 };
